316696e48bf71b9d07d9f2ce893cfcb045ee5b24
morz1ck <enric.bi.06@mail.ru>
2026-08-29 21:53:32 +0300

    adress.py

---
+++ handlers/client/adress.py
@@ -0,0 +1,21 @@
+from handlers import main_router as router
+from forms.user import Form
+from aiogram import F
+from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.context import FSMContext
+from handlers.keyboards import get_streets_keyboard, get_door_keyboard
+
+
+@router.callback_query(Form.address_confirm, F.data == "address_change")
+async def process_address_change(callback: CallbackQuery, state: FSMContext):
+    await state.set_state(Form.street)
+    await callback.message.edit_text(callback.message.text + "\n\n📝 Указываем новый адрес")
+    await callback.message.answer("Выберите улицу:", reply_markup=get_streets_keyboard())
+    await callback.answer()
+
+@router.callback_query(Form.address_confirm, F.data == "address_confirm")
+async def process_address_confirm(callback: CallbackQuery, state: FSMContext):
+    await state.set_state(Form.door_or_concierge)
+    await callback.message.edit_text(callback.message.text + "\n\n✅ Адрес подтверждён")
+    await callback.message.answer("Где оставить пакет?", reply_markup=get_door_keyboard())
+    await callback.answer()
